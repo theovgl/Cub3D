a45799204747510374d394c4dda770c5b6ad5988
ArnaudBD <arnaud.bijuduval@gmail.com>
2022-06-24 16:58:48 +0200

    refactor: floodfill.c norme OK

--- srcs/parsing/floodfill.c
+++ srcs/parsing/floodfill.c
@@ -1,54 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   floodfill.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arnaud <arnaud@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/24 16:55:18 by arnaud            #+#    #+#             */
+/*   Updated: 2022/06/24 16:56:06 by arnaud           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
-void    delfirst_node(t_list **q)
+void	delfirst_node(t_list **q)
 {
-    t_list *temp;
-    temp = *q;
-    *q = (*q)->next;
-    free(temp);
+	t_list	*temp;
+
+	temp = *q;
+	*q = (*q)->next;
+	free(temp);
 }
 
-int floodfill(t_config *conf, t_map m)
+void	add_nodes(t_list *q)
 {
-    int i;
-    int j;
-    t_list *q;
-    t_map map;
+	ft_lstadd_back(&q, ft_lstnew(q->x + 1, q->y));
+	ft_lstadd_back(&q, ft_lstnew(q->x, q->y + 1));
+	if (q->x)
+		ft_lstadd_back(&q, ft_lstnew(q->x - 1, q->y));
+	if (q->y)
+		ft_lstadd_back(&q, ft_lstnew(q->x, q->y - 1));
+}
 
-    map = m;
-    q = ft_lstnew(conf->player.x, conf->player.y);
-printf("q[%d][%d] == %c\n", q->x, q->y, map.map[q->y][q->x]);
-    ft_lstadd_back(&q, ft_lstnew(q->x + 1, q->y));
-    ft_lstadd_back(&q, ft_lstnew(q->x, q->y + 1));
-    if (q->x)
-        ft_lstadd_back(&q, ft_lstnew(q->x - 1, q->y));
-    if (q->y)
-        ft_lstadd_back(&q, ft_lstnew(q->x, q->y - 1));
-    while (ft_lstsize(q))
-    {
-printf("\nQ SIZE == %d\n", ft_lstsize(q));
-printf("for q[%d][%d] == '%c'\n", q->y, q->x, map.map[q->y][q->x]);
-print_map(map);
-printf("Width == %d\tHeight == %d\n", map.map_width, map.map_height);
-        if (map.map[q->y][q->x] == '0')
-        {
-printf("COUCOU!\n");
-            map.map[q->y][q->x] = 'x';
-            ft_lstadd_back(&q, ft_lstnew(q->x + 1, q->y));
-            ft_lstadd_back(&q, ft_lstnew(q->x, q->y + 1));
-            if (q->x)
-                ft_lstadd_back(&q, ft_lstnew(q->x - 1, q->y));
-            if (q->y)
-                ft_lstadd_back(&q, ft_lstnew(q->x, q->y - 1));
-        }
-        if (ft_isspace(map.map[q->y][q->x]) || !ft_isprint(map.map[q->y][q->x]) || (map.map[q->y][q->x] == 'x' && (q->x == 0 || q->x == map.map_width - 1 || q->y == 0 || q->y == map.map_height - 1)))
-        {
-            ft_lstclear(&q, free);
-printf("\n_____________________________________________________________\n");
-            return 1;
-        }
-        delfirst_node(&q);
-    }
-    ft_lstclear(&q, free);
-    return 0;
+void	check_inside(t_map map, t_list *q)
+{
+	if (map.map[q->y][q->x] == '0')
+	{
+		map.map[q->y][q->x] = 'x';
+		add_nodes(q);
+	}
+}
+
+int	floodfill(t_config *conf, t_map m)
+{
+	int		i;
+	int		j;
+	t_list	*q;
+	t_map	map;
+
+	map = m;
+	q = ft_lstnew(conf->player.x, conf->player.y);
+	add_nodes(q);
+	while (ft_lstsize(q))
+	{
+		check_inside(map, q);
+		if (ft_isspace(map.map[q->y][q->x])
+		|| !ft_isprint(map.map[q->y][q->x])
+		|| (map.map[q->y][q->x] == 'x' &&
+		(q->x == 0 || q->x == map.map_width - 1
+		|| q->y == 0 || q->y == map.map_height - 1)))
+		{
+			ft_lstclear(&q, free);
+			return (1);
+		}
+		delfirst_node(&q);
+	}
+	ft_lstclear(&q, free);
+	return (0);
 }
--- srcs/utils/ft_isprint.c
+++ srcs/utils/ft_isprint.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 #include "cub3d.h"
 
-int		ft_isprint(int c)
+int	ft_isprint(int c)
 {
 	if ((c >= 32 && c < 127))
 		return (1);

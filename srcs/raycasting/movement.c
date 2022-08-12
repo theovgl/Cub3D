0aa1e727b084324e0114e02f3bd252ddd335718b
ArnaudBD <arnaud.bijuduval@gmail.com>
2022-08-12 14:00:54 +0200

    fix: movements reseted after fixing the mirrored map pb

--- srcs/raycasting/movement.c
+++ srcs/raycasting/movement.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   movement.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tvogel <tvogel@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abiju-du <abiju-du@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 15:30:22 by tvogel            #+#    #+#             */
-/*   Updated: 2022/08/11 12:23:05 by tvogel           ###   ########.fr       */
+/*   Updated: 2022/08/12 13:50:14 by abiju-du         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,12 @@ void	move_forward(t_player *p, t_map *m)
 
 void	shift(t_player *p, t_map *m)
 {
-	if (check_movement(m, (int)(p->x + p->plane_x
+	if (check_movement(m, (int)(p->x - p->plane_x
 			* (p->strafe_speed)), (int)p->y))
-		p->x += p->plane_x * p->strafe_speed;
-	if (check_movement(m, (int)p->x, (int)(p->y + p->plane_y
+		p->x -= p->plane_x * p->strafe_speed;
+	if (check_movement(m, (int)p->x, (int)(p->y - p->plane_y
 			* (p->strafe_speed))))
-		p->y += p->plane_y * p->strafe_speed;
+		p->y -= p->plane_y * p->strafe_speed;
 }
 
 void	rotate(t_player *p)
@@ -46,12 +46,12 @@ void	rotate(t_player *p)
 
 	old_direction = p->dir_x;
 	old_plane = p->plane_x;
-	p->dir_x = p->dir_x * cos(-p->rotation_speed)
-		- p->dir_y * sin(-p->rotation_speed);
-	p->dir_y = old_direction * sin(-p->rotation_speed)
-		+ p->dir_y * cos(-p->rotation_speed);
-	p->plane_x = p->plane_x * cos(-p->rotation_speed)
-		- p->plane_y * sin(-p->rotation_speed);
-	p->plane_y = old_plane * sin(-p->rotation_speed)
-		+ p->plane_y * cos(-p->rotation_speed);
+	p->dir_x = p->dir_x * cos(p->rotation_speed)
+		- p->dir_y * sin(p->rotation_speed);
+	p->dir_y = old_direction * sin(p->rotation_speed)
+		+ p->dir_y * cos(p->rotation_speed);
+	p->plane_x = p->plane_x * cos(p->rotation_speed)
+		- p->plane_y * sin(p->rotation_speed);
+	p->plane_y = old_plane * sin(p->rotation_speed)
+		+ p->plane_y * cos(p->rotation_speed);
 }
